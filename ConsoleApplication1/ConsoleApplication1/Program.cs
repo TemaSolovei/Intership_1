636dd11b6271d70ef02a3cb15cad83b61957de33
TemaSolovei <temasolovei@gmail.com>
2017-08-20 02:10:07 +0500

    Прописываю проверку слова на ввод

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,17 +13,34 @@ namespace ConsoleApplication1
     class Program
     {
         static int size = 0; // Размер словарного квадрата
-        string[] words; // Массив с заданными словами
+        static string[] words; // Массив с заданными словами
 
         static void Read() // Чтение количества слов и самих слов
         {
             Console.WriteLine("Введите размер словарного квадрата:");
             size = Int32.Parse(Console.ReadLine());
+            words = new string[size*2];
+
+            for (int i = 0; i < size * 2; i++)
+            {
+                string word; // Читаемое слово
+                Console.WriteLine("Введите {0}-е слово:", i+1);
+                while (CheckWord(out word)) words[i] = word;
+            }
         }
 
-        static bool CheckWord()
+        static bool CheckWord(out string readWord)
         {
-            
+            readWord = Console.ReadLine();
+            if (readWord.Length == size)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Длина слова должна быть равна {0} символам", size);
+            }
+            return false;
         }
 
         static void Main(string[] args)
