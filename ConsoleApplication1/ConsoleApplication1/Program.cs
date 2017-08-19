6f11669fc1754f427413fa12a39c2cd0211dca58
TemaSolovei <temasolovei@gmail.com>
2017-08-20 01:45:08 +0500

    Прописал описание и скелет программы

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Соловьев Артём Александрович, ПИ-16-1
+// acmp.ru, задача №611
+// Летняя учебная практика, задача №1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int size = 0; // Размер словарного квадрата
+        string[] words; // Массив с заданными словами
+
+        static void Read() // Чтение количества слов и самих слов
+        {
+            Console.WriteLine("Введите размер словарного квадрата:");
+            size = Int32.Parse(Console.ReadLine());
+        }
+
+        static bool CheckWord()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
