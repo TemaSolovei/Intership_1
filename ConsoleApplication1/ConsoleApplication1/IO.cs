d2f2f49f50e9c55b11225a45e03a70b84367868a
TemaSolovei <temasolovei@gmail.com>
2017-08-22 02:16:44 +0500

    UPD ввод данных

---
+++ ConsoleApplication1/ConsoleApplication1/IO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class IO
+    {
+        static private int sizeOfSquare = 0; // Размер квадрата
+        static private string[] words; // Массив с размером слов
+
+        static void Read()
+        {
+            sizeOfSquare = ReadSize();
+
+        }
+
+        static int ReadSize()
+        {
+            int size;
+
+            Console.WriteLine("Введите размер словарного квадрата:");
+
+            do
+            {
+                size = int.Parse(Console.ReadLine());
+                if ((size > 10) || (size < 2)) Console.WriteLine("Размер квадрата должен быть больше 2 и не должен превышать 10. Введите данные ещё раз");
+            } while ((size > 10) || (size < 2));
+
+            return size;
+        }
+
+        static string[] ReadWords(int numberOfWords)
+        {
+            string[] newWords = new string[numberOfWords];
+            string word;
+            bool checkWord = true;
+
+            for (int i = 0; i < numberOfWords; i++)
+            {
+                do
+                {
+                    Console.WriteLine("Введите {0}-е слово", i + 1);
+                    word = Console.ReadLine();
+
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (word[j] >= 'A' && word[j] <= 'Z') checkWord = true;
+                        else
+                        {
+                            checkWord = false;
+                            Console.WriteLine("В строке должны быть только символы английского алфавита!");
+                            break;
+                        }
+                    }
+                } while (!checkWord);
+
+                newWords[i] = word;
+            }
+        }
+    }
+}
